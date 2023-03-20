0a60c7cc19b6bf0a25eb2a859b9455e3046f3adf
zdeep10 <gabriel.wti02@gmail.com>
2023-03-20 17:54:48 -0300

    Optimizing  service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,44 @@
-const cacheName = 'cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.php',
-  '/assets/css/compressed/custom.min.css',
-  '/vendor/js/custom.js',
-  '/assets/imgs/logo_valida.svg',
-  '/assets/imgs/logo_grupo-polar.svg',
-];
+const cacheVersion = 'v1';
+const cachePrefix = 'cache-';
+const cacheName = cachePrefix + cacheVersion;
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll([]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name.startsWith(cachePrefix) && name !== cacheName)
+          .map((name) => caches.delete(name))
+      );
     })
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.url.startsWith('http')) {
+    event.respondWith(
+      caches.open(cacheName).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          const fetchPromise = fetch(event.request)
+            .then((networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            })
+            .catch(() => {
+              return response;
+            });
+
+          return response || fetchPromise;
+        });
+      })
+    );
+  }
 });
