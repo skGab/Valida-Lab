1dcd33fec36919f741f6c409afd2dad6ab1e2794
zdeep10 <gabriel.wti02@gmail.com>
2023-03-21 16:35:58 -0300

    Getting icons, colors, tipography && optimizing images, customizing scrollbar and scss configs

--- components/header.php
+++ components/header.php
@@ -5,6 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valida Lab</title>
+    <meta name="description" content="O Grupo Polar garante qualidade em toda a cadeia fria, com soluções que asseguram a integridade térmica de produtos durante transporte e armazenamento. Com mais de 20 anos de experiência e certificação ISO 9001:2015, oferecemos tecnologia e inovação para aprimorar a logística em todo o Brasil e no mundo. Conte com nossos serviços especializados para garantir a qualidade de seus medicamentos durante todo o ciclo de vida.">
+
+    <!-- TIPOGRAFIA -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- END TIPOGRAFIA -->
 
     <!-- FAV ICON V2 -->
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/pwa/apple-touch-icon.png">
@@ -14,14 +22,11 @@
     <meta name="theme-color" content="#053565">
     <!-- END FAV ICON V2 -->
 
-    <title>Grupo Polar</title>
-    <meta name="description" content="O Grupo Polar garante qualidade em toda a cadeia fria, com soluções que asseguram a integridade térmica de produtos durante transporte e armazenamento. Com mais de 20 anos de experiência e certificação ISO 9001:2015, oferecemos tecnologia e inovação para aprimorar a logística em todo o Brasil e no mundo. Conte com nossos serviços especializados para garantir a qualidade de seus medicamentos durante todo o ciclo de vida.">
-
     <!-- BOOTSTRAP 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- CUSTOM CSS -->
-    <link rel="stylesheet" href="../vendor/scss/custom.scss">
+    <link rel="stylesheet" href="/assets/css/compressed/custom.min.css">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php require('./components/header.php') ?>
 
 <section>
-    <h1>Hello World!</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Hello World!</h1>
+                <p>Nessa paleta de cores, o --back-ground é um cinza claro, o --text é um cinza escuro, o --title-h1 é um branco um pouco mais suave, o --title-h2-white é um branco quase igual ao --back-ground e o --title-h3 é um preto mais suave. Essas cores podem ser ajustadas ainda mais de acordo com a necessidade do projeto.</p>
+            </div>
+        </div>
+    </div>
+    <!-- <object type="image/svg+xml" data="/assets/icons/simbolo.svg"></object> -->
 </section>
 
 <?php require('./components/footer.php') ?>
